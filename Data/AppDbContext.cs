07a6aebd9836e99883e7a66a34c77173279ca759
Erik Pires <169729132+ekpires@users.noreply.github.com>
2025-09-08 16:44:32 -0300

    migration setup

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using INVENTAR.IO.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace INVENTAR.IO.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected AppDbContext()
+        {
+        }
+
+        public virtual DbSet<Sector> Sector { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using INVENTAR.IO.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
