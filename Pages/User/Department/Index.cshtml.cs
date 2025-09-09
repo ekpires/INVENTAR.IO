50d3bcbafde2d3954b49f2c0a8e4cecc2caedf14
Erik Pires <169729132+ekpires@users.noreply.github.com>
2025-09-09 17:29:07 -0300

    implemented reading from db

--- Pages/User/Department/Index.cshtml.cs
+++ Pages/User/Department/Index.cshtml.cs
@@ -1,12 +1,25 @@
+using INVENTAR.IO.Data;
+using INVENTAR.IO.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace INVENTAR.IO.Pages.User.Department
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly AppDbContext _context;
+
+        public IndexModel(AppDbContext context)
         {
+            _context = context;
+        }
+
+        public IList<Departments> Departments { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Departments = await _context.Departments.ToListAsync();
         }
     }
 }
