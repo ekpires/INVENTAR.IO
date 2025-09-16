016054d4eaf272b7bd607cfcbbbb5cb005d803c5
Erik Pires <169729132+ekpires@users.noreply.github.com>
2025-09-16 17:19:33 -0300

    created colaborators model

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -17,8 +17,11 @@ namespace INVENTAR.IO.Data
         {
             modelBuilder.Entity<Departments>().HasKey(e => e.DepartmentId);
             modelBuilder.Entity<Departments>().Property(e =>  e.DepartmentId).ValueGeneratedOnAdd();
+            modelBuilder.Entity<Colaborators>().HasKey(e => e.ColaboratorId);
+            modelBuilder.Entity<Colaborators>().Property(e => e.ColaboratorId).ValueGeneratedOnAdd();
         }
 
         public virtual DbSet<Departments> Departments { get; set; }
+        public virtual DbSet<Colaborators> Colaborators { get; set; }
     }
 }
---
+++ Models/Colaborators.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace INVENTAR.IO.Models
+{
+    [Index(nameof(ColaboratorEmail), IsUnique = true)]
+    public class Colaborators
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ColaboratorId { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string ColaboratorName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(128)]
+        public string ColaboratorEmail { get; set; } = string.Empty;
+
+        [Required]
+        [ForeignKey(nameof(Departments.DepartmentId))]
+        public int DepartmentId { get; set; }
+        public virtual Departments Department { get; set; } = default!;
+    }
+}
