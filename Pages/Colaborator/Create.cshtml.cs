c93aebbe7ee7c5e444a1d6e6cf0b98835998eded
Erik Pires <169729132+ekpires@users.noreply.github.com>
2025-10-02 11:43:19 -0300

    create feature implemented

--- Pages/Colaborator/Create.cshtml.cs
+++ Pages/Colaborator/Create.cshtml.cs
@@ -1,12 +1,63 @@
+using INVENTAR.IO.Data;
+using INVENTAR.IO.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Diagnostics;
 
 namespace INVENTAR.IO.Pages.Colaborator
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+
+        private readonly AppDbContext _context;
+
+        public CreateModel(AppDbContext context)
         {
+            _context = context;
+        }
+        public List<SelectListItem> Departments { get; set; } = default!;
+
+        public IActionResult OnGet()
+        {
+
+            Departments = _context.Departments.Select(departments => new
+            SelectListItem
+            {
+                Text = departments.DepartmentName,
+                Value = departments.DepartmentId.ToString()
+
+            }).ToList();
+
+            return Page();
+        }
+
+        [BindProperty]
+        public Colaborators Colaborator { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Colaborator.ColaboratorName.ToUpper();
+            Colaborator.ColaboratorEmail.ToLower();
+
+            try
+            {
+                _context.Colaborators.Add(Colaborator);
+                await _context.SaveChangesAsync();
+            }
+            catch
+            {
+                TempData["Message"] = $"{Colaborator.ColaboratorName} already exists";
+            }
+
+            return RedirectToPage("./Index");
         }
     }
 }
