b7bda2d2452c03815c35e61cf16f0b7a95ba2e54
Erik Pires <169729132+ekpires@users.noreply.github.com>
2025-10-22 10:33:59 -0300

    changed produc index path to root index page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,27 @@
+using INVENTAR.IO.Data;
+using INVENTAR.IO.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace INVENTAR.IO.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly AppDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
+        
+        public IList<Products> Products { get; set; } = default!;
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-
+            Products = await _context.Products.ToListAsync();
         }
     }
 }
