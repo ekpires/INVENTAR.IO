8c4431709eb78e0eb8723e4c78bce1229e5204ca
Erik Pires <169729132+ekpires@users.noreply.github.com>
2025-11-15 18:26:04 -0300

    implemented asset migration

--- Migrations/20251115212111_AssetMigration.Designer.cs
+++ Migrations/20251115212111_AssetMigration.Designer.cs
@@ -3,6 +3,7 @@ using INVENTAR.IO.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace INVENTAR.IO.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251115212111_AssetMigration")]
+    partial class AssetMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20251115212111_AssetMigration.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INVENTAR.IO.Migrations
+{
+    /// <inheritdoc />
+    public partial class AssetMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Assets",
+                columns: table => new
+                {
+                    AssetId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ColaboratorId = table.Column<int>(type: "int", nullable: true),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assets", x => x.AssetId);
+                    table.ForeignKey(
+                        name: "FK_Assets_Colaborators_ColaboratorId",
+                        column: x => x.ColaboratorId,
+                        principalTable: "Colaborators",
+                        principalColumn: "ColaboratorId");
+                    table.ForeignKey(
+                        name: "FK_Assets_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assets_AssetId",
+                table: "Assets",
+                column: "AssetId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assets_ColaboratorId",
+                table: "Assets",
+                column: "ColaboratorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assets_ProductId",
+                table: "Assets",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Assets");
+        }
+    }
+}
