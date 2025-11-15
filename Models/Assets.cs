019b809cef6d8a502f19e27e8fad718eae56ecd6
Erik Pires <169729132+ekpires@users.noreply.github.com>
2025-11-15 18:02:18 -0300

    created assets model

---
+++ Models/Assets.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace INVENTAR.IO.Models
+{
+    [Index(nameof(AssetId), IsUnique = true)]
+    public class Assets
+    {
+        [Key, Required, DisplayName("Asset Code")]
+        public string AssetId{ get; set; } = string.Empty;
+
+        [DisplayName("Owner"), ForeignKey(nameof(Colaborators.ColaboratorId))]
+        public int? ColaboratorId { get; set; }
+        public virtual Colaborators Colaborator { get; set; } = default!;
+    
+        [Required, ForeignKey(nameof(Products.ProductId)), DisplayName("Product")]
+        public int ProductId { get; set; }
+        public virtual Products Product {  get; set; } = default!;
+    }
+}
