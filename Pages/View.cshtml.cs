da52e9fec8e8784a42d1d5cd945ac644fdb745d8
Erik Pires <169729132+ekpires@users.noreply.github.com>
2025-12-01 09:22:38 -0300

    implements assets in view page

--- Pages/View.cshtml.cs
+++ Pages/View.cshtml.cs
@@ -13,25 +13,33 @@ namespace INVENTAR.IO.Pages
         [BindProperty]
         public Products Product { get; set; } = default!;
 
-        public ViewModel(AppDbContext context)
+        [BindProperty]
+        public IList<Assets> Assets { get; set; } = default!;
+
+        public ViewModel( AppDbContext context )
         {
             _context = context;
         }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        public async Task<IActionResult> OnGetAsync( int? id )
         {
-            if (id == null)
-                return RedirectToPage("./Index");
+            if( id == null )
+                return RedirectToPage( "./Index" );
 
-            var product = await _context.Products.FindAsync(id);
+            var product = await _context.Products.FindAsync( id );
 
-            if (product == null)
-                return RedirectToPage("./Index");
-            else 
-            {
-                Product = product;
-                return Page();
-            }
+            if( product == null )
+                return RedirectToPage( "./Index" );
+
+            Product = product;
+
+            Assets = await _context.Assets
+                .Include(c => c.Colaborator)
+                .Include(c => c.Colaborator.Department)
+                .Where(a => a.ProductId == id).ToListAsync();
+
+
+            return Page();
         }
     }
 }
